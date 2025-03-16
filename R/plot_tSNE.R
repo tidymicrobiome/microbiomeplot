9ad20614260cbd35b7d7c4a55e30d26ac3d785ec
sahanarahim <sahanarahim2804@gmail.com>
2025-03-16 12:32:35 +0800

    update

--- R/plot_tSNE.R
+++ R/plot_tSNE.R
@@ -1,28 +1,33 @@
 #' @title Plotting a t-SNE plot for visualizing high-dimensional microbiome data
 #'
 #' @param object the microbiomedataset object
-#' @param legend legend for the plot 
+#' @param legend string; legend for the plot 
 #' @param title Title of the plot
-#' @param theme theme for the ggplot 
-#' @param legend.position position of the legend 
-#' @param size size of the labels 
-#' @param grad the column of the sample_info to be colored by
-#' @param dims the output dimensionality of the t-sne object
-#' @param perplexity A parameter that controls the balance between local and global aspects of the data when t-SNE is creating the low-dimensional map.
-#' @param number_of_iteration Number of iterations (default: 1000)
-#' @param initial_dims e number of dimensions that should be retained in the initial PCA step
-#' @param pca ogical; Whether an initial PCA step should be performed
+#' @param theme The theme of the ggplot to be displayed
+#' - These themes include:
+#'    - theme_minimal
+#'    - theme_classic
+#'    - theme_bw
+#'    - theme_light
+#'    - theme_dark
+#' @param legend.position position of the legend ("bottom", "top", "right", "left")
+#' @param size numeric; size of the labels (default: 2)
+#' @param grad the column of the sample_info to be colored by (no default)
+#' @param dims the output dimensions of the t-sne object (default: 2)
+#' @param perplexity A parameter that controls the balance between local and global aspects of the data when t-SNE is creating the low-dimensional map 
+#' @param number_of_iters numeric; Number of iterations (default: 1000)
+#' @param initial_dims numeric; the number of dimensions that should be retained in the initial PCA step
+#' @param pca logical; Whether an initial PCA step should be performed (default: TRUE)
 #' @param x_title The x-axis title of the plot 
 #' @param y_title the y-axis title of the plot
 #' @param title_size the size of the text for the title
 #' @param z_title  the z-axis title of the plot
 #' @param .. other parameters 
 #'
-#' @return A ggplo2 class object
-#' 
+#' @return A ggplot2 class object
 #' @export
 plot_tSNE <- function(object, grad, dims = 2,  
-                       perplexity = 30, number_of_iteration = 1000, initial_dims = 50, pca = TRUE, 
+                      perplexity = NULL , number_of_iters = 1000, initial_dims = 50, pca = TRUE, 
                        legend = TRUE, title = "t-SNE plot for Microbiome Data",
                        x_title = "dimension 1", y_title = "dimension 2", z_title = "dimension 3", theme = "minimal", legend.position = "bottom", 
                        size = 3, title_size = 10, ..){
@@ -37,8 +42,18 @@ plot_tSNE <- function(object, grad, dims = 2,
 #' @importFrom microbiomedataset extract_expression_data extract_sample_info
 #' @importFrom plotly plot_ly
 #' @export
+#' @examples
+#' 
+#' plot_tSNE (global_patterns, grad = "SampleType", perplexity = NULL , 
+#' number_of_iters  = 1000, initial_dims = 50, pca = TRUE, 
+#' legend = TRUE, title = "t-SNE plot for Microbiome Data",
+#' x_title = "Dimension 1 of the tsne plot", y_title = "Dimension 2 of the tsne plot", 
+#' z_title = "Dimension 3 of the tsne plot", theme = "minimal", 
+#' legend.position = "bottom", 
+#' size = 3, title_size = 10) 
+
 plot_tSNE.microbiome_dataset  <- function(object, grad, dims = 2,  
-                                         perplexity = 30, number_of_iters = 1000, initial_dims = 50, pca = TRUE, 
+                                          perplexity = NULL , number_of_iters = 1000, initial_dims = 50, pca = TRUE, 
                                          legend = TRUE, title = "t-SNE plot for Microbiome Data",
                                          x_title = "dimension 1", y_title = "dimension 2", z_title = "dimension 3", theme = "minimal", legend.position = "bottom", 
                                          size = 3, title_size = 10, ..){
@@ -46,13 +61,18 @@ plot_tSNE.microbiome_dataset  <- function(object, grad, dims = 2,
   expression_data <- microbiomedataset::extract_expression_data(object)
   sample_info <- microbiomedataset::extract_sample_info(object)
   
+  max_perplexity <- (ncol(expression_data) - 1) / 3
+
+  if (is.null(perplexity)) {
+    perplexity <- max_perplexity - 1  # Default perplexity
+    perplexity <- round(perplexity, 6)  # Round to 6 decimal places for consistency
+  }
+  
   if (!grad %in% colnames(sample_info)) {
     stop("The column '", grad, "' does not exist in the sample_info data.")
   }
   
-  expression_data <- t(expression_data)
-  
-  tSNE_object <- Rtsne::Rtsne(expression_data, 
+  tSNE_object <- Rtsne::Rtsne(X = t(expression_data), 
                               initial_dims = initial_dims, 
                               dims = dims, 
                               perplexity = perplexity, 
@@ -93,7 +113,7 @@ return(tsne_plot)
   } 
   else if(dims == 3){
     
-    tSNE_object <- Rtsne::Rtsne(expression_data, 
+    tSNE_object <- Rtsne::Rtsne(X = t(expression_data),  
                                 initial_dims = initial_dims, 
                                 dims = dims, 
                                 perplexity = perplexity, 
@@ -116,26 +136,13 @@ return(tsne_plot)
       "Dimension 3:", round(tsne_df$Z, 3)
     )
     
-    tsne_plot <- plot_ly(
-      data = tsne_df,
-      x = ~X,
-      y = ~Y,
-      z = ~Z,
-      type = "scatter3d",
-      mode = "markers",
-      marker = list(size = 6),
-      text = hover_text,
-      hoverinfo = "text",
-      color = ~Sample
-    ) %>%
-      layout(
-        title = "t-SNE 3-Dimensional Digit Visualization",
-        scene = list(
-          xaxis = list(title = "t-SNE Dimension 1"),
-          yaxis = list(title = "t-SNE Dimension 2"),
-          zaxis = list(title = "t-SNE Dimension 3")
-        )
-      )
+    tsne_plot <- plotly::plot_ly( data = tsne_df, x = ~X, y = ~Y, z = ~Z, 
+                                  type = "scatter3d", mode = "markers", 
+                                  marker = list(size = size), text = hover_text,
+                                  hoverinfo = "text", color = ~Sample ) %>% 
+      plotly::layout( title = title, scene = list( xaxis = list(title = x_title), 
+                                                   yaxis = list(title = y_title), 
+                                                   zaxis = list(title = z_title) ) )
     
     return(tsne_plot)
     
